4e5bb4c55c8bd439264a52abddf8e8077b0dcdcc
Jim Wooley <JimWooley@hotmail.com>
2014-09-08 23:45:06 -0400

    Add test project

---
+++ Repair.Tests/RecipeContextTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeDal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Repair.Tests
+{
+    [TestClass]
+    public class RecipeContextTests
+    {
+        [TestMethod]
+        public void Context_CanCreateDatabase()
+        {
+            var targetPath = @"C:\projects\temp\testdb.mdf";
+            try
+            {
+                if (File.Exists(targetPath))
+                    File.Delete(targetPath);
+            }
+            catch
+            {
+            }
+
+            using (var context = new RecipeContext(@"Server =.; AttachDbFilename =" + targetPath + "; Database = RecipeMdf; Trusted_Connection = Yes;"))
+            {
+                Assert.IsFalse(context.Recipes.Any());
+            }
+            Assert.IsTrue(File.Exists(targetPath));
+        }
+    }
+}
