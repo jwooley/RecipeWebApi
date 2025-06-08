485ae6bf40df75ea51c4038e3626449fc13f98eb
JIMXPS13\jimwo <jimwooley@hotmail.com>
2025-06-08 15:54:13 -0400

    Fix winform launch

---
+++ RecipeForm/MyApplication.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+
+Namespace My
+    Partial Friend Class MyApplication
+        Inherits WindowsFormsApplicationBase
+
+        Protected Overrides Sub OnCreateMainForm()
+            MainForm = New Form1()
+        End Sub
+    End Class
+End Namespace
